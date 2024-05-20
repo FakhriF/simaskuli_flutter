d2fa27bbc61f78e7dbeca4ca6b7084cf9f570b28
Sakuya3rd <ahmadnd00@protonmail.com>
2024-05-20 15:37:53 +0700

    Improved Forum UI (Use ListView Builder)

--- lib/pages/forum/forum_page.dart
+++ lib/pages/forum/forum_page.dart
@@ -1,10 +1,12 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ForumPage extends StatelessWidget {
   const ForumPage({super.key});
 
-  
+  static const List<String> title = ['Lorem', 'Ipsum', 'dolor']; // Temp Data
+  static const List<String> name = ['Albert', 'Bekky', 'Clarisse']; // Temp Data
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -21,75 +23,64 @@ class ForumPage extends StatelessWidget {
               ),
 
             ),
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              child: Card(
-                clipBehavior: Clip.antiAlias,
-                child: ListView.separated(
-                  padding: const EdgeInsets.symmetric(vertical: 2),
-                  shrinkWrap: true,
-                  children: [
-
-                    ListTile(
-                      onTap: () => {
-
-                        debugPrint("You clicked on this thread!"),
-                      },
-                      leading: Icon(Icons.people),
-                      title: const Text('Name'),
-                      subtitle: Text(
-                        'By {name}, MMM dd, yyyy',
-                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.thumb_up, color: Colors.blue, size: 20),
-                        onPressed: () => {
-                          debugPrint("You liked this Thread!"),
+            Expanded(
+              child: ListView.builder(
+                itemCount: name.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Card(
+                      clipBehavior: Clip.antiAlias,
+                      child: ListTile(
+                        contentPadding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                        onTap: () {
+                          debugPrint("You clicked on this thread!");
                         },
+                        leading: Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              color: Colors.blue,
+                              width: 2.0,
+                            ),
+                          ),
+                          child: Icon(
+                            Icons.people,
+                            color: Colors.blue,
+                          ),
+                        ),
+                        title: Text(
+                          '${title[index]}',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                          'By ${name[index]}, MMM dd, yyyy',
+                          style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                        ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.thumb_up, color: Colors.blue, size: 20),
+                              onPressed: () {
+                                debugPrint("You liked this Thread!");
+                              },
+                            ),
+                            Text('Likes', style: TextStyle(color: Colors.blue)) , // Example text
+                          ],
+                        ),
+
                       ),
                     ),
-                    Divider(),
-
-                    ListTile(
-                      onTap: () => {
-
-                        debugPrint("You clicked on this thread!"),
-                      },
-                      leading: Icon(Icons.people),
-                      title: const Text('Name'),
-                      subtitle: Text(
-                        'By {name}, MMM dd, yyyy',
-                        style: TextStyle(color: Colors.black.withOpacity(0.6)),
-                      ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.thumb_up, color: Colors.blue, size: 20),
-                        onPressed: () => {
-                          debugPrint("You liked this Thread!"),
-                        },
-                      ),
-                    ),
-                  ],
-                ),
+                  );
+                },
               ),
             )
-
-          ]
-
-
-
+          ],
         ),
-        // child: Center(
-        //   child: Column(
-        //       crossAxisAlignment: CrossAxisAlignment.center,
-        //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        //       children: [
-        //         Text("Test")
-        //       ]
-        //
-        //   ),
-        // ),
       ),
-
     );
   }
 }
